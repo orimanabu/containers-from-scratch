fb35c24760373dfcdd89892d6e2fb50a38da1d89
Manabu Ori <manabu.ori@gmail.com>
2024-12-27 14:42:56 +0900

    update

--- c/main.c
+++ c/main.c
@@ -13,6 +13,7 @@
 
 #define STACK_SIZE 1024 * 1024
 #define ROOTFS "/home/ori/ubuntu-rootfs"
+#define BUFSIZE 1024
 
 struct cmd_arg {
 	int argc;
@@ -44,35 +45,6 @@ int into_miscns(void *arg)
 		return 1;
 	}
 
-	printf("* xxx 1\n");
-	struct cmd_arg *cmdarg = (struct cmd_arg *)arg;
-	printf("* xxx 1.1\n");
-	char **p = (char **)malloc(sizeof(char *) * (cmdarg->argc + 1));
-	printf("* xxx 1.2\n");
-	const char *prog = cmdarg->argv[1];
-
-	/*
-	printf("* xxx 2\n");
-	int i;
-	for (i = 0; i < cmdarg->argc; i++) {
-		printf("  %d: [%s]\n", i, cmdarg->argv[i+1]);
-		p[i] = cmdarg->argv[i+1];
-	}
-	printf("* xxx 3\n");
-	printf("XXX execv start: [%s]", prog);
-	for (i = 0; i < cmdarg->argc-1; i++) {
-		printf(" %s", p[i]);
-	}
-	printf("\n");
-
-	int exec_ret = execv(prog, p);
-	if (exec_ret == -1) {
-		printf("execv failed: %s\n", strerror(errno));
-		return 1;
-	}
-	printf("XXX execv end\n");
-	*/
-
 	execlp("bash", "bash", NULL);
 	perror("execlp failed");
 	return -1;
@@ -84,6 +56,10 @@ int into_userns(void *arg)
 {
 	struct cmd_arg *cmdarg = (struct cmd_arg *)arg;
 	int i;
+	int ret;
+	char buf[1024];
+	char map_path[BUFSIZE];
+	pid_t pid = getpid();
 
 	printf("Child1 (into_userns): PID = %d, PPID = %d\n", getpid(), getppid());
 
@@ -95,18 +71,10 @@ int into_userns(void *arg)
 	const char *gid_map = "0 1000 1\n";
 	FILE *uid_map_file, *gid_map_file;
 
-	if ((uid_map_file = fopen("/proc/self/uid_map", "w")) == NULL) {
-		perror("Failed to open /proc/self/uid_map");
-		return -1;
-	}
-	if (fwrite(uid_map, 1, strlen(uid_map), uid_map_file) != strlen(uid_map)) {
-		perror("Failed to write to /proc/self/uid_map");
-		fclose(uid_map_file);
-		return -1;
-	}
-	fclose(uid_map_file);
+	system("/usr/bin/id");
 
-	if ((gid_map_file = fopen("/proc/self/gid_map", "w")) == NULL) {
+	sprintf(map_path, "/proc/%d/gid_map", pid);
+	if ((gid_map_file = fopen(map_path, "w")) == NULL) {
 		perror("Failed to open /proc/self/gid_map");
 		return -1;
 	}
@@ -117,8 +85,19 @@ int into_userns(void *arg)
 	}
 	fclose(gid_map_file);
 
-	printf("UID and GID mappings applied\n");
-	printf("Now running as unprivileged user\n");
+	sprintf(map_path, "/proc/%d/uid_map", pid);
+	if ((uid_map_file = fopen(map_path, "w")) == NULL) {
+		perror("Failed to open /proc/self/uid_map");
+		return -1;
+	}
+	if (fwrite(uid_map, 1, strlen(uid_map), uid_map_file) != strlen(uid_map)) {
+		perror("Failed to write to /proc/self/uid_map");
+		fclose(uid_map_file);
+		return -1;
+	}
+	fclose(uid_map_file);
+
+	system("/usr/bin/id");
 
 	char *stack2 = malloc(STACK_SIZE);
 	if (!stack2) {
@@ -132,8 +111,7 @@ int into_userns(void *arg)
 	    exit(EXIT_FAILURE);
 	}
 
-	//waitpid(pid2, NULL, 0);
-	wait(NULL);
+	waitpid(pid2, NULL, 0);
 	free(stack2);
 
 	printf("Child1: Exiting after Child2.\n");
@@ -151,13 +129,13 @@ int main(int argc, char *argv[])
     }
 
     pid_t pid1 = clone(into_userns, stack1 + STACK_SIZE, CLONE_NEWUSER|SIGCHLD, (void *)&cmdarg);
+    //pid_t pid1 = clone(into_userns, stack1 + STACK_SIZE, SIGCHLD, (void *)&cmdarg);
     if (pid1 == -1) {
 	    perror("clone child1");
 	    exit(EXIT_FAILURE);
     }
 
-    //waitpid(pid1, NULL, 0);
-    wait(NULL);
+    waitpid(pid1, NULL, 0);
     free(stack1);
 
     printf("Parent: children have exited.\n");
